5a6a29f1785a781fac6c8aee6db7cbe1fc1bbcd8
TWulfZ <andyvelezespejo@gmail.com>
2024-09-10 15:39:08 -0500

    Chapter 5: Navigation (next/link)

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      Hello world, I'm the dashboard page
+    </div>
+  )
+}
+
+export default page
---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter, Montserrat, Lusitana } from "next/font/google";
+export const inter = Inter({ subsets: ["latin"] });
+export const monserrat = Montserrat({ subsets: ["latin"] });
+export const lusitana = Lusitana({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
